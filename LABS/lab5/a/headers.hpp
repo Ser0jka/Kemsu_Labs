ae11a909ec5bb19263c6eaec72e6bc44dc2a2bd3
Сергей Суровцев <sersur42@MacBook-Air-Sergej.local>
2026-03-12 11:02:15 +0700

    upd

--- LABS/lab5/a/headers.hpp
+++ LABS/lab5/a/headers.hpp
@@ -1,19 +1,19 @@
 #pragma once
 #include <string>
 
-// Функция для генерации чисел и записи в общий файл
+// функция для генерации чисел и записи в общий файл
 void generator(int& lenMinusArr, int& lenPlusArr);
 
 int* makeArray(int& lenArr);
 
-// Функция для чтения массива из файла
+// функция для чтения массива из файла
 void readArrayFromFile(int* arr, int size, std::string fileName);
 
-// Функция для вывода массива на экран
+// функция для вывода массива на экран
 void printArray(const int* arr, int size);
 
-// Функция для вывода Максимального отрицательного числа и его индекса
+// функция для вывода максимального отрицательного числа и его индекса
 void MaxMinus(int* arr, int& maxMinusNum, int& pos, int& lenMinusArr);
 
-// Функция для вывода Минимального положительного числа и его индекса
+// функция для вывода минимального положительного числа и его индекса
 void MinPlus(int* arr, int& MinPlusNum, int& pos, int& lenPlussArr);
